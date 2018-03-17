0a9231dcaae0e99451518aeeb4a6ca221597a903
ihsanfikri12 <fikriabiyyu@gmail.com>
2018-03-18 02:52:37 +0700

    love you

---
+++ latihan1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+	int main () {
+	
+	int C;
+	int i;
+	
+	for (C=1;C<10;C++) {
+		for (i=1;i<10;i++) {
+			cout<<C*i<<' ';
+		} 
+		cout << endl;
+	}
+
+
+
+}
